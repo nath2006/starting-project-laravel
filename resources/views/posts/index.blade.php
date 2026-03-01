9e1486186830d9fd4579b1b61d819cb8cd60910d
Nath2006 <ananthapindika739@gmail.com>
2026-03-01 10:14:29 +0700

    update ui post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,9 +12,27 @@
         <th>Title</th>
         <th>Aksi</th>
     </tr>
+
+    @foreach ($posts as $post){
+    <tr>
+        <td>{{ $loop ->iteration }}</td>
+        <td>{{ $loop->title }}</td>
+        <td>
+            <a href="{{ route('posts.edit') }}" class="btn btn-warning mb-3">Edit</a>
+            <form action="{{ route('posts.destroy') }}" method="POST" class="d-inline"
+                  onsubmit="return confirm('Yakin mau hapus postingan ini?')"
+            >
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+            </form>
+        </td>
+    </tr>
+}
+@endforeach
+
 </table>
 
-
-
+{{ $posts->link() }}
 @endsection
 
